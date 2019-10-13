052f2ab7a12f2cd4ee9ceb4c2e53508a3126107c
Faeez Shaikh <faeez.shaikh@gmail.com>
2019-10-12 20:08:18 -0500

    Added decks

--- src/app/decks/decks.page.ts
+++ src/app/decks/decks.page.ts
@@ -8,8 +8,14 @@ import { CardDetailsPage } from '../card-details/card-details.page';
   styleUrls: ['./decks.page.scss'],
 })
 export class DecksPage implements OnInit {
+  decks = [{}];
   ngOnInit(): void {
+    this.decks.push({id:1,name:"Deck 1"},{id:3,name:"Deck 3"})
     // throw new Error("Method not implemented.");
+  }
+
+  openDeck(deckId) {
+    
   }
 
   constructor(public modalController: ModalController) {}
